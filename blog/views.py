4ad0bd14c90dd15d3907500582d1227acb9fc8f4
d0mk <dominik.kobyra@gmail.com>
2019-11-17 01:52:25 +0100

    templates and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Autor1',
+        'title': 'blog post 1',
+        'content': 'first post content',
+        'date_posted': '17.11.2019'
+    },
+    {
+        'author': 'Autor2',
+        'title': 'blog post 2',
+        'content': 'second post content',
+        'date_posted': '18.11.2019'
+    }
+]
+
+
+def home(request):
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
